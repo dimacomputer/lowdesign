f0a2d1c68c19f3b748ded340fbb6a64fb51250ba
Dmitry Litvinov <dimacomputer@gmail.com>
2025-10-22 13:40:54 +0200

    давайте вернем бутстрап

--- inc/assets/ld-bootstrap-bridge.php
+++ inc/assets/ld-bootstrap-bridge.php
@@ -13,14 +13,13 @@ add_action(
         $theme_dir = get_stylesheet_directory();
         $theme_uri = get_stylesheet_directory_uri();
 
-        $css_path = $theme_dir . "/vendor/bootstrap/dist/css/bootstrap.min.css";
-        $js_path =
-            $theme_dir . "/vendor/bootstrap/dist/js/bootstrap.bundle.min.js";
+        $css_path = $theme_dir . "/vendor/bootstrap/css/bootstrap.min.css";
+        $js_path = $theme_dir . "/vendor/bootstrap/js/bootstrap.bundle.min.js";
 
         if (file_exists($css_path)) {
             wp_enqueue_style(
                 "bootstrap",
-                $theme_uri . "/vendor/bootstrap/dist/css/bootstrap.min.css",
+                $theme_uri . "/vendor/bootstrap/css/bootstrap.min.css",
                 [],
                 filemtime($css_path),
             );
@@ -29,8 +28,7 @@ add_action(
         if (file_exists($js_path)) {
             wp_enqueue_script(
                 "bootstrap",
-                $theme_uri .
-                    "/vendor/bootstrap/dist/js/bootstrap.bundle.min.js",
+                $theme_uri . "/vendor/bootstrap/js/bootstrap.bundle.min.js",
                 [],
                 filemtime($js_path),
                 true,
